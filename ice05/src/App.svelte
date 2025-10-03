42063fff4e31f8852b96faee740133a4023c363f
Ethan Carter <ebcarter96@gmail.com>
2025-10-03 16:42:09 -0400

    yes

--- ice05/src/App.svelte
+++ ice05/src/App.svelte
@@ -1,47 +1,47 @@
+
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let msgs = []
+
+  const ws = new WebSocket( 'ws://127.0.0.1:3000' )
+
+  // when connection is established...
+  ws.onopen = () => {
+    ws.send( 'a new client has connected.' )
+    
+    ws.onmessage = async msg => {
+      // add message to end of msgs array,
+      // re-assign to trigger UI update
+      const message = await msg.data.text()
+      let body = document.getElementById('app')
+      try{
+        body.style.backgroundColor = message.trim()
+      }
+      catch{
+
+      }
+      msgs = msgs.concat([ 'them: ' + message ])
+    }
+    
+  }
+
+  const send = function() {
+    const txt = document.querySelector('input').value
+    ws.send( txt )
+    msgs = msgs.concat([ 'me: ' + txt ])
+  }
 </script>
+<div>
+  <input type='text' >
+  <input type='submit' value='submit' on:click={send}><br>
+</div>
+  
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<!-- {#await promise then something}
+{/await} -->
 
-  <div class="card">
-    <Counter />
-  </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+{#each msgs as msg }
+  <h3>{msg}</h3><br><br>
+{/each}
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
