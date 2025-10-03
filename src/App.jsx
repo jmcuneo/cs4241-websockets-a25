279c7320c87db6b7adcd3c592a53b08095580b47
christopheryon <203912328+christopheryon@users.noreply.github.com>
2025-10-03 15:55:49 -0400

    Random colors and communicate colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,61 @@
-import { useEffect, useState, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 
-
 function App() {
-  const [count, setCount] = useState(0)
-  const [msgs, setMsgs] = useState([])
-  let ws = null
-  let ctx = null
+    const [count, setCount] = useState(0)
+    const [msgs, setMsgs] = useState([])
+    let ws = null
+    let ctx = null
 
-  useEffect(() => {
-    ws = new WebSocket( 'ws://127.0.0.1:3000' )
+    useEffect(() => {
+        ws = new WebSocket('ws://127.0.0.1:3000')
 
-    ws.onopen = () => {
-      ws.onmessage = async msg => {
-        const pos = await msg.data.text()
-        const [x,y] = pos.split( ':' ).map( v => parseInt(v) )
+        ws.onopen = () => {
+            ws.onmessage = async msg => {
+                const pos = await msg.data.text()
+                const data = JSON.parse(pos)
+                const x = data.x
+                const y = data.y
 
-        ctx.fillStyle = 'red'
-        ctx.fillRect( x,y,50,50 )
-      }
-    }
 
-    const canvas = document.querySelector('canvas')
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-    ctx = canvas.getContext( '2d' )
+                ctx.fillStyle = data.color
+                ctx.fillRect(x, y, 50, 50)
+            }
+        }
 
-    window.onclick = e => {
-          ws.send( `${e.pageX}:${e.pageY}` )
-          ctx.fillStyle = 'yellow'
-          ctx.fillRect( e.pageX,e.pageY,50,50 )
-    }
-  })
+        const canvas = document.querySelector('canvas')
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        ctx = canvas.getContext('2d')
 
-  return (
-    <>
-        <canvas></canvas>
-    </>
-  )
+        function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        const color = getRandomColor()
+        window.onclick = e => {
+            const data = {
+                x: e.pageX,
+                y: e.pageY,
+                color: color
+            }
+            ws.send(JSON.stringify(data))
+            ctx.fillStyle = color
+            ctx.fillRect(e.pageX, e.pageY, 50, 50)
+        }
+    })
+
+    return (
+        <>
+            <canvas></canvas>
+        </>
+    )
 }
 
 export default App
