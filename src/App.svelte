9b8285f7ebcec348c44b0d3202691f457f981b1f
MikeLin0321 <mzlin@wpi.edu>
2025-10-03 15:48:51 -0400

    User can now enter their name on the input box on the left.

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,42 @@
 <script>
   let msgs = []
-      
-  const ws = new WebSocket( 'ws://127.0.0.1:3000' )
+  // bind username and input text to Svelte variables
+  let username = ''
+  let txt = ''
+
+  // initialize username from localStorage if available
+  if (typeof localStorage !== 'undefined') {
+    username = localStorage.getItem('username') || ''
+  }
+
+  const ws = new WebSocket('ws://127.0.0.1:3000')
 
   // when connection is established...
   ws.onopen = () => {
-    ws.send( 'a new client has connected.' )
-
-    ws.onmessage = async msg => {
-      // add message to end of msgs array,
-      // re-assign to trigger UI update
-      const message = await msg.data.text()
-      msgs = msgs.concat([ 'them: ' + message ])
-    }
+    ws.send('a new client has connected.')
   }
 
-  const send = function() {
-    const txt = document.querySelector('input').value
-    ws.send( txt )
-    msgs = msgs.concat([ 'me: ' + txt ])
+  // handle incoming messages
+  ws.onmessage = async msg => {
+    const message = await msg.data.text()
+    msgs = msgs.concat([message])
+  }
+
+  // persist username whenever it changes
+  $: if (typeof localStorage !== 'undefined') {
+    localStorage.setItem('username', username)
+  }
+
+  function send() {
+    if (!txt) return
+    ws.send(username + ':' + txt)
+    msgs = msgs.concat(['me: ' + txt])
+    txt = ''
   }
 </script>
 
-<input type='text' on:change={send} />
+<input type='text' id='username' bind:value={username} placeholder='username' />
+<input type='text' id='input' bind:value={txt} on:change={send} placeholder='message' />
 
 {#each msgs as msg }
   <h3>{msg}</h3>
