691ad95d997a340efc0156c0b3dc66fd41c6508c
rempixel <yjiang00634@gmail.com>
2025-10-03 15:29:57 -0400

    server setup

---
+++ src/client/main.jsx
@@ -0,0 +1,14 @@
+import App from './App';
+function MainApp() {
+
+
+  return (
+   <App></App>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MainApp />
+  </React.StrictMode>,
+);
