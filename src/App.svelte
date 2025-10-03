3bf0f42f5fc444cde3e9017838a571ba758bd71a
cacsibal <cacsibal@gmail.com>
2025-10-03 16:06:31 -0400

    moved to root directory

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,45 @@
 <script>
-  let msgs = []
-  // bind username and input text to Svelte variables
-  let username = ''
-  let txt = ''
+    let msgs = []
+    // bind username and input text to Svelte variables
+    let username = ''
+    let txt = ''
 
-  // initialize username from localStorage if available
-  if (typeof localStorage !== 'undefined') {
-    username = localStorage.getItem('username') || ''
-  }
+    // initialize username from localStorage if available
+    if (typeof localStorage !== 'undefined') {
+        username = localStorage.getItem('username') || ''
+    }
 
-  const ws = new WebSocket('ws://127.0.0.1:3000')
+    const ws = new WebSocket('ws://127.0.0.1:3000')
 
-  // when connection is established...
-  ws.onopen = () => {
-    ws.send('a new client has connected.')
-  }
+    // when connection is established...
+    ws.onopen = () => {
+        ws.send('a new client has connected.')
+    }
 
-  // handle incoming messages
-  ws.onmessage = async msg => {
-    const message = await msg.data.text()
-    msgs = msgs.concat([message])
-  }
+    // handle incoming messages
+    ws.onmessage = async msg => {
+        const message = await msg.data.text()
+        msgs = msgs.concat([message])
+    }
 
-  // persist username whenever it changes
-  $: if (typeof localStorage !== 'undefined') {
-    localStorage.setItem('username', username)
-  }
+    // persist username whenever it changes
+    $: if (typeof localStorage !== 'undefined') {
+        localStorage.setItem('username', username)
+    }
 
-  function send() {
-    if (!txt) return
-    ws.send(username + ':' + txt)
-    msgs = msgs.concat(['me: ' + txt])
-    txt = ''
-  }
+    function send() {
+        if (!txt) return
+        ws.send(username + ': ' + txt)
+        msgs = msgs.concat(['me: ' + txt])
+        txt = ''
+
+        console.log(msgs)
+    }
 </script>
 
 <input type='text' id='username' bind:value={username} placeholder='username' />
 <input type='text' id='input' bind:value={txt} on:change={send} placeholder='message' />
 
 {#each msgs as msg }
-  <h3>{msg}</h3>
+    <h3>{msg}</h3>
 {/each}
