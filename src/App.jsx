2db5497d0ff8935456878af8aedf16afad081858
ceciherriman <herrimanceci@gmail.com>
2025-10-03 15:43:30 -0400

    canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,56 +7,38 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-    const ws = new WebSocket( 'ws://127.0.0.1:3000' )
-    const [msgs, setMsgs] = useState([])
-// when connection is established...
+  const [msgs, setMsgs] = useState([])
+  let ws = null
+  let ctx = null
+
+  useEffect(() => {
+    ws = new WebSocket( 'ws://127.0.0.1:3000' )
+
     ws.onopen = () => {
-        ws.send( 'a new client has connected.' )
+      ws.onmessage = async msg => {
+        const pos = await msg.data.text()
+        const [x,y] = pos.split( ':' ).map( v => parseInt(v) )
 
-        ws.onmessage = async msg => {
-            // add message to end of msgs array,
-            // re-assign to trigger UI update
-            const message = await msg.data.text()
-            setMsgs(  msgs.concat([ 'them: ' + message ]))
-        }
+        ctx.fillStyle = 'red'
+        ctx.fillRect( x,y,50,50 )
+      }
     }
 
-    const send = function() {
-        const txt = document.querySelector('input').value
-        ws.send( txt )
-        setMsgs(msgs.concat([ 'me: ' + txt ]))
+    const canvas = document.querySelector('canvas')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    ctx = canvas.getContext( '2d' )
+
+    window.onclick = e => {
+          ws.send( `${e.pageX}:${e.pageY}` )
+          ctx.fillStyle = 'yellow'
+          ctx.fillRect( e.pageX,e.pageY,50,50 )
     }
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-        <input type='text' onChange={send} />
-        {
-            msgs.map((msg) =>{
-               return (<p>
-                   {msg}
-                </p>)
-            })
-        }
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <canvas></canvas>
     </>
   )
 }
